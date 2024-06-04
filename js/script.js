435c88ce44c3bec9afd7f060568d03492282fdf0
L1LZ4Z <joaquinorozco2004@gmail.com>
2024-06-04 09:15:44 -0500

    Prueba numero no me acuerdo

--- js/script.js
+++ js/script.js
@@ -1,45 +1,55 @@
 // Funcion de arranque. Asigna comportamientos de forma dinamica al HTML
-(function(window) {
-  // Carga el form principal
-    
-  // Comportamiento de botones de barra nav
-  var boton_nav1 = document.getElementById('load-about-us');
-  var boton_nav2 = document.getElementById('load-why-us');
-  var boton_nav3 = document.getElementById('load-contact-us');
-  
-  // Event listener para botones
-  boton_nav1.addEventListener('click', function(event) {
-    event.preventDefault();
-    fetch('snippets/snippet_about_us.html')
-              .then(response => response.text())
-              .then(data => {
-                document.getElementById('main-content').innerHTML = data;
-                loadDefaultForm();
-              })
-              .catch(error => {
-                console.error('Error al cargar el contenido dinamico del formulario:', error);
-              }); 
-  });
-  boton_nav2.addEventListener('click', function(event) {
+document.addEventListener('DOMContentLoaded', function() {
+  (function() {
+
+    // Verificación de la ejecución de la IIFE al cargar el DOM
+    console.log("Se cargo el DOM y se ejecuta la IIFE");
+
+    // Identificar botones
+    var boton_nav1 = document.getElementById('load-about-us');
+    var boton_nav2 = document.getElementById('load-why-us');
+    var boton_nav3 = document.getElementById('load-contact-us');
+
+    // Event listener para botones
+    boton_nav1.addEventListener('click', function(event) {
+      event.preventDefault();
+      fetch('snippets/snippet_about_us.html')
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById('main-content').innerHTML = data;
+        })
+        .catch(error => {
+          console.error('Error al cargar el contenido dinamico del formulario:', error);
+        });
+    });
+
+    boton_nav2.addEventListener('click', function(event) {
       event.preventDefault();
       fetch('snippets/snippet_why_us.html')
-                .then(response => response.text())
-                .then(data => {
-                  document.getElementById('main-content').innerHTML = data;
-                })
-                .catch(error => {
-                  console.error('Error al cargar el contenido dinamico del formulario:', error);
-                });
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById('main-content').innerHTML = data;
+        })
+        .catch(error => {
+          console.error('Error al cargar el contenido dinamico del formulario:', error);
+        });
     });
-        boton_nav3.addEventListener('click', function(event) {
-    event.preventDefault();
-    fetch('snippets/snippet_contact_us.html')
-              .then(response => response.text())
-              .then(data => {
-                document.getElementById('main-content').innerHTML = data;
-              })
-              .catch(error => {
-                console.error('Error al cargar el contenido dinamico del formulario:', error);
-              });
-  });
-})(window);
+
+    boton_nav3.addEventListener('click', function(event) {
+      event.preventDefault();
+      fetch('snippets/snippet_contact_us.html')
+        .then(response => response.text())
+        .then(data => {
+          document.getElementById('main-content').innerHTML = data;
+        })
+        .catch(error => {
+          console.error('Error al cargar el contenido dinamico del formulario:', error);
+        });
+    });
+
+    // Simulamos un click en la primera opcion del nav para que se cargue el contenido principal
+    var event = new Event('click');
+    boton_nav1.dispatchEvent(event);
+    
+  })(); // IIFE
+});     // Evento asignado
